0f18e0909aab979cbe8afc2cbfcd2905d66822e1
OtaraAlex <mbechealex61@gmail.com>
2024-12-21 00:05:37 +0300

    setup fonts and assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router'
 const Index = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text className='font-bold text-lg my-10'>Welcome to ReState</Text>
+      <Text className='font-bold text-3xl my-10 font-rubik'>Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
