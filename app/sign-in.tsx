14c12d3c0192efca9898ad0cd487814926f4ab99
OtaraAlex <mbechealex61@gmail.com>
2024-12-21 14:45:33 +0300

    feat: integrate authentication

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
+import { Redirect } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
 
 const SignIn = () => {
-  const handleLogin = () => {};
+  const { refetch, loading, isLoggedIn }= useGlobalContext();
+    
+    if(!loading && isLoggedIn) return <Redirect href="/" />;
+
+  const handleLogin = async () => {
+    const result = await login();
+
+    if(result) {
+        refetch();
+    } else {
+        Alert.alert('Error', 'Failed to login');
+    }
+};
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
